5a5390d3edf511a773a8df4aff4ed4ce1f16fb96
wangqianwen0418 <wangqianwen0418@163.com>
2021-10-13 17:06:01 -0400

    feat: grid images to show latent space

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,10 +3,17 @@ import './App.css';
 
 import Grid from './components/Grid';
 
+const latentDim = 7
+const images = Array.from(
+    Array(latentDim).keys()
+  ).map(row_idx=>Array.from(
+      Array(11).keys()
+    ).map(col_idx=>`assets/simu/${row_idx}_${col_idx}.png`))
+
 function App() {
   return (
     <div className="App">
-      <Grid images= {[['']]}/>
+      <Grid images= {images}/>
     </div>
   );
 }
--- front/src/components/Grid.tsx
+++ front/src/components/Grid.tsx
@@ -10,9 +10,10 @@ export default class Grid extends React.Component <Props, States> {
     render(){
         return <div className='grid'>
             <h4> Latent Space </h4>
-            {this.props.images.map((row,i) =>{
-                return <div className='row' key={`row_${i}`}>
-                    {row.map(url=><img key="url" src={url} className={styles.latent} alt={url} />)}
+            {this.props.images.map((row,row_idx) =>{
+                return <div className='row' key={`row_${row_idx}`}>
+                    <span className={styles.dimHeader}>DIM_{row_idx}</span>
+                    {row.map((url, col_idx)=><img key={`${row_idx}_${col_idx}`} src={url} className={styles.latentImage} alt={url} />)}
                 </div>
             })}
             </div>
