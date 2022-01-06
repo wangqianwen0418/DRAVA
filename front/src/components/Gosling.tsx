009ebc40a46271bcf41889ca9ff3178575b49f63
wangqianwen0418 <wangqianwen0418@163.com>
2022-01-05 22:44:17 -0500

    feat: rows for different level lables

--- front/src/components/Gosling.tsx
+++ front/src/components/Gosling.tsx
@@ -5,77 +5,121 @@ import { Card } from 'antd';
 import { TResultRow } from 'types';
 
 interface Props {
-    samples: TResultRow[];
-    width: number;
-    height: number;
+  samples: TResultRow[];
+  dataset: string;
+  width: number;
+  height: number;
 }
 
 export const GoslingVis = (props: Props) => {
-    const labelJSON = props.samples.map(sample => {
-        return {
-            Chromosome: `chr${sample.chr}`,
-            chromStart: sample.start,
-            chromEnd: sample.end
-        };
+  const labelJSON = props.samples
+    // .filter(sample => !sample.level || sample.level == '3.0') // TO-DO, test using only level-3 or no level labels
+    .map(sample => {
+      return {
+        chromosome: `chr${sample.chr}`,
+        chromStart: sample.start,
+        chromEnd: sample.end,
+        ...sample
+      };
     });
-    const rootStyle = getComputedStyle(document.documentElement),
-        cardPadding = parseInt(rootStyle.getPropertyValue('--card-body-padding')),
-        cardHeadHeight = parseInt(rootStyle.getPropertyValue('--card-head-height'));
 
-    const spec = {
-        title: '',
-        width: props.width - cardPadding * 2,
-        height: props.height - cardPadding * 2 - cardHeadHeight - 24, // gosling vis axis: 24px
-        tracks: [
-            {
-                layout: 'linear',
-                data: {
-                    url: 'https://s3.amazonaws.com/gosling-lang.org/data/HFFc6_Atacseq.mRp.clN.bigWig',
-                    type: 'bigwig',
-                    column: 'position',
-                    value: 'peak',
-                    binSize: '2'
-                },
-                mark: 'area',
-                x: {
-                    field: 'position',
-                    type: 'genomic',
-                    domain: { chromosome: '7' },
-                    axis: 'bottom'
-                },
-                y: { field: 'peak', type: 'quantitative' },
-                color: { value: 'black' },
-                height: 40
-            },
-            {
-                data: {
-                    values: labelJSON,
-                    type: 'json',
-                    chromosomeField: 'Chromosome',
-                    genomicFields: ['chromStart', 'chromEnd']
-                },
-                mark: 'rect',
-                size: { value: 12 },
-                height: 12,
-                x: { field: 'chromStart', type: 'genomic', axis: 'none', domain: { chromosome: '7' } },
-                xe: { field: 'chromEnd', type: 'genomic' },
-                stroke: { value: 'orange' },
-                strokeWidth: { value: 1 }
-            }
-        ]
-    };
+  const rootStyle = getComputedStyle(document.documentElement),
+    cardPadding = parseInt(rootStyle.getPropertyValue('--card-body-padding')),
+    cardHeadHeight = parseInt(rootStyle.getPropertyValue('--card-head-height'));
 
-    // validate the spec
-    const validity = validateGoslingSpec(spec);
+  const labelTrack: any = {
+    data: {
+      values: labelJSON,
+      type: 'json',
+      chromosomeField: 'chromosome',
+      genomicFields: ['start', 'end'],
+      categories: ['1.0', '2.0', '3.0']
+    },
+    mark: 'rect',
+    size: { value: 12 },
+    height: 12,
+    x: {
+      field: 'start',
+      type: 'genomic',
+      axis: 'none',
+      domain: { chromosome: labelJSON[labelJSON.length - 1].chromosome }
+    },
+    xe: { field: 'end', type: 'genomic' },
+    stroke: { value: 'orange' },
+    strokeWidth: { value: 1 }
+  };
 
-    if (validity.state === 'error') {
-        console.warn('Gosling spec is invalid!', validity.message);
-        return <></>;
-    }
+  if (props.dataset == 'matrix') {
+    labelTrack['row'] = { field: 'level', type: 'nominal', legend: false };
+    labelTrack['height'] = 12 * 6;
+    labelTrack['stroke'] = { value: 'white' };
+  }
 
-    return (
-        <Card title="Genomic Browser" size="small">
-            <GoslingComponent spec={spec as GoslingSpec} />
-        </Card>
-    );
+  const MatrixTrack = {
+    title: 'HFFc6_Hi-C',
+    data: {
+      url: 'https://server.gosling-lang.org/api/v1/tileset_info/?d=hffc6-hic-hg38',
+      type: 'matrix'
+    },
+    mark: 'rect',
+    x: {
+      field: 'position1',
+      type: 'genomic',
+      axis: 'bottom'
+    },
+    y: {
+      field: 'position2',
+      type: 'genomic',
+      axis: 'none'
+    },
+    color: {
+      field: 'value',
+      type: 'quantitative',
+      range: 'grey'
+    },
+    width: 600,
+    height: 600
+  };
+
+  const PeakTrack = {
+    layout: 'linear',
+    data: {
+      url: 'https://s3.amazonaws.com/gosling-lang.org/data/HFFc6_Atacseq.mRp.clN.bigWig',
+      type: 'bigwig',
+      column: 'position',
+      value: 'peak',
+      binSize: '2'
+    },
+    mark: 'area',
+    x: {
+      field: 'position',
+      type: 'genomic',
+      domain: { chromosome: '7' },
+      axis: 'bottom'
+    },
+    y: { field: 'peak', type: 'quantitative' },
+    color: { value: 'black' },
+    height: 40
+  };
+
+  const spec = {
+    title: '',
+    width: props.width - cardPadding * 2,
+    height: props.height - cardPadding * 2 - cardHeadHeight - 24, // gosling vis axis: 24px
+    tracks: [props.dataset == 'sequence' ? PeakTrack : MatrixTrack, labelTrack]
+  };
+
+  // validate the spec
+  const validity = validateGoslingSpec(spec);
+
+  if (validity.state === 'error') {
+    console.warn('Gosling spec is invalid!', validity.message);
+    return <></>;
+  }
+
+  return (
+    <Card title="Genomic Browser" size="small">
+      <GoslingComponent spec={spec as GoslingSpec} />
+    </Card>
+  );
 };
