7230c124202a1fae42ff2f98774ef3db7afa46de
wangqianwen0418 <wangqianwen0418@163.com>
2022-01-27 15:51:08 -0500

    feat: increase image contrast

--- front/scripts/change_colormap.py
+++ front/scripts/change_colormap.py
@@ -1,24 +1,34 @@
 #%%
-from PIL import Image
+from PIL import Image, ImageEnhance
 from matplotlib import cm as colormap
 import numpy as np
 
 import os
 
 #%%
-def color_convert(filename, mapname='viridis'):
+def color_convert(filename, savename=None, mapname='viridis', factor=1.5):
+    if savename == None:
+        savename = filename
     cm = colormap.get_cmap(mapname)
     img_src = Image.open(filename).convert('L')
-    img_src.thumbnail((512,512))
-    im = np.array(img_src)
+
+    # increase contrast
+    enhancer = ImageEnhance.Contrast(img_src)
+    im = enhancer.enhance(factor)
+
+
+    im = np.array(im)
     im = cm(im)
     im = np.uint8(im * 255)
     im = Image.fromarray(im)
-    im.save(filename)
 
-file_folder = '../public/assets/matrix_simu'
+    im.save(savename)
+
+#%%
+file_folder = '../public/assets/matrix_simu_copy'
 for f in os.listdir(file_folder):
     filepath = os.path.join(file_folder, f)
-    color_convert(filepath)
+    savepath = filepath.replace('/matrix_simu_copy/', '/matrix_simu/')
+    color_convert(filepath, savepath)
 
 # %%
