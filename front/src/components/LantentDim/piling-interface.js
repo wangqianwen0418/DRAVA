67a08071ef2089e6e1342e5a310bfdc4224498ce
wangqianwen0418 <wangqianwen0418@163.com>
2022-02-16 10:33:53 -0500

    feat: improving piling interface

--- front/src/components/LantentDim/piling-interface.js
+++ front/src/components/LantentDim/piling-interface.js
@@ -5,9 +5,9 @@ import createPilingJs from 'piling.js';
  * @param   {string}  src  Remote image source, i.e., a URL
  * @return  {object}  Promise resolving to the image once its loaded
  */
-export const loadImage = (src, isCrossOrigin = false) =>
+export const loadImage = (src, option, isCrossOrigin = false) =>
     new Promise((resolve, reject) => {
-        const image = new Image(64, 64);
+        const image = new Image(option.imageSize, option.imageSize);
         if (isCrossOrigin) image.crossOrigin = 'Anonymous';
         image.onload = () => {
             resolve(image);
@@ -20,18 +20,31 @@ export const loadImage = (src, isCrossOrigin = false) =>
         image.style.border = '1px solid gray';
     });
 
-const createImageRenderer = () => sources =>
+const createImageRenderer = option => sources =>
     Promise.all(
         sources.map(src => {
             const isCrossOrigin = true;
-            return loadImage(src, isCrossOrigin);
+            return loadImage(src, option, isCrossOrigin);
         })
     );
 
 export default async function create(element, items, pileDragEnd) {
+    const imageSize = 32;
+
     const piling = createPilingJs(element, {
-        renderer: createImageRenderer(),
-        items: items
+        renderer: createImageRenderer({ imageSize }),
+        items: items,
+        // pileBorderSize: 2,
+        // pileOpacity: 0.5 //opaciy piles for the sequence dataset
+        pileItemOffset: (item, i, pile) => {
+            const isNotLast = pile.items.length - 1 !== i;
+            return [+isNotLast * (Math.random() * 12 - 6), +isNotLast * (Math.random() * 12 - 6)];
+        },
+        pileItemRotation: (item, i, pile) => {
+            const isNotLast = pile.items.length - 1 !== i;
+            return +isNotLast * (Math.random() * 12 - 6);
+        },
+        pileSizeBadge: pile => pile.items.length > 1
     });
     piling.arrangeBy('data', ['x', 'y']);
     piling.groupBy('overlap');
