34b20fdaff445718d42bf392f6b530218316f05f
wangqianwen0418 <wangqianwen0418@163.com>
2022-02-15 23:30:03 -0500

    customize render

--- front/src/components/LantentDim/piling-interface.js
+++ front/src/components/LantentDim/piling-interface.js
@@ -1,4 +1,32 @@
-import createPilingJs, { createImageRenderer } from 'piling.js';
+import createPilingJs from 'piling.js';
+
+/**
+ * Promised-based image loading
+ * @param   {string}  src  Remote image source, i.e., a URL
+ * @return  {object}  Promise resolving to the image once its loaded
+ */
+export const loadImage = (src, isCrossOrigin = false) =>
+    new Promise((resolve, reject) => {
+        const image = new Image(64, 64);
+        if (isCrossOrigin) image.crossOrigin = 'Anonymous';
+        image.onload = () => {
+            resolve(image);
+        };
+        image.onerror = error => {
+            console.error(`Could't load ${src}`);
+            reject(error);
+        };
+        image.src = src;
+        image.style.border = '1px solid gray';
+    });
+
+const createImageRenderer = () => sources =>
+    Promise.all(
+        sources.map(src => {
+            const isCrossOrigin = true;
+            return loadImage(src, isCrossOrigin);
+        })
+    );
 
 export default async function create(element, items, pileDragEnd) {
     const piling = createPilingJs(element, {
