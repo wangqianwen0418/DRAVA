e33f089941d9ae4fc0b19c7e28442b59dd943e57
wangqianwen0418 <wangqianwen0418@163.com>
2022-03-27 10:57:24 -0400

    feat: scale canvas to fit

--- front/src/components/ImageContext.tsx
+++ front/src/components/ImageContext.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Card } from 'antd';
+import { Card, Slider } from 'antd';
 import { TResultRow } from 'types';
 import { BASE_URL } from 'Const';
-import { ImageResource } from 'pixi.js';
 import { getMax } from 'helpers';
 
 interface Props {
@@ -16,6 +15,8 @@ interface Props {
 const ImageContext = (props: Props) => {
   const { isDataLoading, samples, height, width, dataset } = props;
 
+  const [zoom, changeZoom] = useState(1);
+
   const rootStyle = getComputedStyle(document.documentElement),
     cardHeadHeight = parseInt(rootStyle.getPropertyValue('--card-head-height'));
 
@@ -28,13 +29,17 @@ const ImageContext = (props: Props) => {
     return { x, y, url };
   });
 
-  const canvasWidth = getMax(imgSamples.map(s => s.x)),
-    canvasHeight = getMax(imgSamples.map(s => s.y));
+  const canvasOverallWidth = getMax(imgSamples.map(s => s.x)),
+    canvasOverallHeight = getMax(imgSamples.map(s => s.y)),
+    canvasHeight = height - cardHeadHeight;
 
+  // draw images
   useEffect(() => {
     const canvas: any = canvasRef.current;
     if (canvas == null) return;
     const ctx = canvas.getContext('2d');
+    const scale = (zoom * width) / canvasOverallWidth;
+    ctx.scale(scale, scale);
 
     imgSamples.forEach(sample => {
       const image = new Image(50, 50); // Using optional size for image
@@ -45,18 +50,37 @@ const ImageContext = (props: Props) => {
     });
 
     return () => {
-      ctx.clearRect(0, 0, width, canvasHeight);
+      ctx.clearRect(0, 0, width, height - cardHeadHeight);
     };
   }, [samples]);
 
+  //   // update canvas zoom
+  //   useEffect(() => {
+  //     const canvas: any = canvasRef.current;
+  //     if (canvas == null) return;
+  //     const ctx = canvas.getContext('2d');
+  //     console.info(zoom, ctx);
+  //     ctx.scale(zoom, zoom);
+  //   }, [zoom]);
+
   return (
     <Card
-      title={`Image Context`}
+      title={`Context View`}
       size="small"
       bodyStyle={{ overflow: 'scroll', height: height - cardHeadHeight }}
       loading={isDataLoading}
+      //   extra={
+      //     <Slider
+      //       min={1}
+      //       max={10}
+      //       step={0.1}
+      //       value={zoom / (width / canvasOverallWidth)}
+      //       style={{ width: 100 }}
+      //       onChange={v => changeZoom(v * (width / canvasOverallWidth))}
+      //     />
+      //   }
     >
-      <canvas id="imageContext" ref={canvasRef} width={canvasWidth} height={canvasHeight}></canvas>
+      <canvas id="imageContext" ref={canvasRef} width={width} height={canvasHeight}></canvas>
     </Card>
   );
 };
