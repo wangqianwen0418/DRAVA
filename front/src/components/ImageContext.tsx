8164f2675f0a584662f1610d9042f3f944abc25f
wangqianwen0418 <wangqianwen0418@163.com>
2022-03-27 11:10:14 -0400

    feat: add filter masks for image context

--- front/src/components/ImageContext.tsx
+++ front/src/components/ImageContext.tsx
@@ -3,6 +3,7 @@ import { Card, Slider } from 'antd';
 import { TResultRow } from 'types';
 import { BASE_URL } from 'Const';
 import { getMax } from 'helpers';
+import { image } from 'd3-fetch';
 
 interface Props {
   isDataLoading: boolean;
@@ -15,18 +16,16 @@ interface Props {
 const ImageContext = (props: Props) => {
   const { isDataLoading, samples, height, width, dataset } = props;
 
-  const [zoom, changeZoom] = useState(1);
-
   const rootStyle = getComputedStyle(document.documentElement),
     cardHeadHeight = parseInt(rootStyle.getPropertyValue('--card-head-height'));
 
   const canvasRef = useRef(null);
   const imgSamples = samples.map(s => {
-    const id = s.id;
+    const { id, filtered } = s;
     const x = parseInt(id.match(/(?<=_x)\d+/)![0]);
     const y = parseInt(id.match(/(?<=_y)\d+/)![0]);
     const url = `${BASE_URL}/api/get_${dataset}_sample?id=${id}`;
-    return { x, y, url };
+    return { x, y, url, filtered };
   });
 
   const canvasOverallWidth = getMax(imgSamples.map(s => s.x)),
@@ -38,7 +37,7 @@ const ImageContext = (props: Props) => {
     const canvas: any = canvasRef.current;
     if (canvas == null) return;
     const ctx = canvas.getContext('2d');
-    const scale = (zoom * width) / canvasOverallWidth;
+    const scale = width / canvasOverallWidth;
     ctx.scale(scale, scale);
 
     imgSamples.forEach(sample => {
@@ -52,16 +51,23 @@ const ImageContext = (props: Props) => {
     return () => {
       ctx.clearRect(0, 0, width, height - cardHeadHeight);
     };
-  }, [samples]);
+  }, [samples.length]);
 
-  //   // update canvas zoom
-  //   useEffect(() => {
-  //     const canvas: any = canvasRef.current;
-  //     if (canvas == null) return;
-  //     const ctx = canvas.getContext('2d');
-  //     console.info(zoom, ctx);
-  //     ctx.scale(zoom, zoom);
-  //   }, [zoom]);
+  // draw image mask
+  useEffect(() => {
+    const canvas: any = canvasRef.current;
+    if (canvas == null) return;
+    const ctx = canvas.getContext('2d');
+    imgSamples
+      .filter(d => d.filtered)
+      .forEach(sample => {
+        ctx.beginPath();
+        ctx.globalAlpha = 0.5;
+        ctx.fillStyle = 'white';
+        ctx.fillRect(sample.x - 50, sample.y - 50, 50, 50);
+        ctx.globalAlpha = 1.0;
+      });
+  }, samples);
 
   return (
     <Card
@@ -69,16 +75,6 @@ const ImageContext = (props: Props) => {
       size="small"
       bodyStyle={{ overflow: 'scroll', height: height - cardHeadHeight }}
       loading={isDataLoading}
-      //   extra={
-      //     <Slider
-      //       min={1}
-      //       max={10}
-      //       step={0.1}
-      //       value={zoom / (width / canvasOverallWidth)}
-      //       style={{ width: 100 }}
-      //       onChange={v => changeZoom(v * (width / canvasOverallWidth))}
-      //     />
-      //   }
     >
       <canvas id="imageContext" ref={canvasRef} width={width} height={canvasHeight}></canvas>
     </Card>
