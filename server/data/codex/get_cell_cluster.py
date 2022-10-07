e17e764e5a384f974af7185693164fe94e7794e4
wangqianwen0418 <wangqianwen0418@163.com>
2022-10-07 17:32:45 -0400

    feat: change downsample + cell boundaries

--- server/data/codex/get_cell_cluster.py
+++ server/data/codex/get_cell_cluster.py
@@ -22,6 +22,13 @@ down_sampling = 2
 w = h = 64
 shift_step = 20
 
+#%%
+def split_2d(array, splits):
+    h, w = splits
+    b = np.stack(np.split(array, w, axis=2), axis=0)
+    c = np.concatenate(np.split(b, h, axis=2))
+    return c
+
 # %%
 
 # numpy array, shape (29, 7491, 12664)
@@ -44,41 +51,55 @@ grid_h = math.floor(tif.shape[1]/h)
 grid_w = math.floor(tif.shape[2]/w)
 
 new_tif = tif[selected_channel_index, :grid_h*h, :grid_w*w]
-def split_2d(array, splits):
-    h, w = splits
-    b = np.stack(np.split(array, w, axis=2), axis=0)
-    c = np.concatenate(np.split(b, h, axis=2))
-    return c
 cell_grids = split_2d(new_tif, (grid_h, grid_w))
+
 
 for shift in range(0, min(w, h), shift_step):
 
     new_tif_shift = tif[selected_channel_index, shift:(grid_h-1)*h + shift, shift:(grid_w-1)*w+shift]
-
-
     grids_shift = split_2d(new_tif_shift, (grid_h-1, grid_w-1))
     cell_grids = np.concatenate( (cell_grids, grids_shift), axis=0)
 
 # normalize
 patch_size = (cell_grids.shape[0], len(selected_channel_index), h, w)
 norm_cell_grids = zarr.zeros(patch_size, chunks=(
-    100, None, None, None), dtype='float32')
+    1000, None, None, None), dtype='float32')
 for i,c in enumerate(selected_channels):
     v_min, v_max = selected_channels[c]
     clip_grids = np.clip(cell_grids[:, i, :, :], v_min, v_max)
     norm_cell_grids[:, i, :, :] = (clip_grids- v_min) / (v_max - v_min)
 
+zarr.save(f'{foldername}/cell_grids_d{down_sampling}_s{shift_step}.zarr', norm_cell_grids)
 
-zarr.save(f'{foldername}/cell_grids.zarr', norm_cell_grids)
+#%% for each item, save them cell boundaries
+tif_mask = imread(f'{foldername}/reg1_stitched_mask.ome.tif')
+# 4 indicates cell mask, nuclei masks, cell boundaries, and nucleus boundaries
+cell_boundries = tif_mask[2:3]
+cell_boundries = cell_boundries[:, ::down_sampling, ::down_sampling]
+
+cell_boundries = cell_boundries[:, :grid_h*h, :grid_w*w]
+
+cell_boundries_grids = split_2d(cell_boundries, (grid_h, grid_w))
+for shift in range(0, min(w, h), shift_step):
+    cell_boundries_shift = cell_boundries[:, shift:(grid_h-1)*h + shift, shift:(grid_w-1)*w+shift]
+    cell_boundries_grids_shift = split_2d(cell_boundries_shift, (grid_h-1, grid_w-1))
+    cell_boundries_grids = np.concatenate( (cell_boundries_grids, cell_boundries_grids_shift), axis=0)
+
+zarr.save(f'{foldername}/cell_masks_d{down_sampling}_s{shift_step}.zarr', cell_boundries_grids)
 # %%
 # visualize
 
 
-def visualize(cell_id: int):
-    z = zarr.open(f'{foldername}/cell_grids.zarr', mode='r')
-    a = z[cell_id]
+def visualize(item_id: int):
+    z = zarr.open(f'{foldername}/cell_grids_d{down_sampling}_s{shift_step}.zarr', mode='r')
+    a = z[item_id]
+
+    masks = zarr.open(f'{foldername}/cell_masks_d{down_sampling}_s{shift_step}.zarr', mode='r')
+    mask = masks[item_id]
+
     plt.figure(figsize=(10, 10))
-    color_lists = [ 'Reds','Greens', 'Blues']
+    
+    # a subfigure for each antigen chanel
     for i in range(a.shape[0]):
         ax = plt.subplot(5, 6, i + 1)
         ax.axis("off")
@@ -86,13 +107,21 @@ def visualize(cell_id: int):
         rgb_img[:,:, i] = a[i,:,:]
 
         ax.imshow(rgb_img)
+    
+    # a subfigure for three channels
     ax = plt.subplot(5, 6, a.shape[0]+1)
     ax.axis("off")
+    ax.imshow(a.transpose(1, 2, 0))
+
+    # a subfigure for cell bounders
+    ax = plt.subplot(5, 6, a.shape[0]+2)
+    ax.axis("off")
+    a[:, mask[0]>0] = 1
     ax.imshow(a.transpose(1, 2, 0))
     plt.tight_layout()
 
 # %%
 visualize(4000)
-visualize(8000)
+visualize(5000)
 
 # %%
