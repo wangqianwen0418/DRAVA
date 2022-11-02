6f777c46c68fb1c751cb5fd12abe8a2b396f15a6
wangqianwen0418 <wangqianwen0418@163.com>
2022-11-02 10:47:23 -0400

    refactor

--- server/data/codex/get_cell_grids.py
+++ server/data/codex/get_cell_grids.py
@@ -22,9 +22,11 @@ selected_channels = {'DAPI-02': [930,3153], 'CD20': [125,9012], 'CD21':[10246,65
 exp_filename = 'reg001_expr.ome.tif'
 mask_filename = 'reg001_mask.ome.tif'
 
-zoom_level = 1 # 0 indicates the most detailed view
+filter = True
+
+zoom_level = 0 # 0 indicates the most detailed view
 w = h = 64
-shift_step = 20
+shift_step = 10
 
 #%%
 def split_2d(array, splits):
@@ -69,7 +71,6 @@ for i,c in enumerate(selected_channels):
     clip_grids = np.clip(cell_grids[:, i, :, :], v_min, v_max)
     norm_cell_grids[:, i, :, :] = (clip_grids- v_min) / (v_max - v_min)
 
-zarr.save(f'{foldername}/cell_grids_level{zoom_level}_step{shift_step}.zarr', norm_cell_grids)
 
 #%% for each item, save them cell boundaries
 tif_mask = imread(f'{foldername}/{mask_filename}', level=zoom_level)
@@ -84,25 +85,29 @@ for shift in range(0, min(w, h), shift_step):
     cell_boundries_grids_shift = split_2d(cell_boundries_shift, (grid_h-1, grid_w-1))
     cell_boundries_grids = np.concatenate( (cell_boundries_grids, cell_boundries_grids_shift), axis=0)
 
-zarr.save(f'{foldername}/cell_masks_level{zoom_level}_step{shift_step}.zarr', cell_boundries_grids)
 #%%
-# # filter out black item
-v_mask = np.amax(norm_cell_grids, axis=(1,2,3))>0.5
-# filter out non-cell grids
-c_mask = np.amax(cell_boundries_grids, axis=(1,2,3))>0
-mask = v_mask & c_mask
-filter_cell_grids = cell_grids[mask]
-patch_size = (filter_cell_grids.shape[0], len(selected_channel_index), h, w)
+if not filter:
+    zarr.save(f'{foldername}/cell_grids_level{zoom_level}_step{shift_step}.zarr', norm_cell_grids)
 
-norm_filter_cell_grids = zarr.zeros(patch_size, chunks=(
-    1000, None, None, None), dtype='float32')
-for i,c in enumerate(selected_channels):
-    v_min, v_max = selected_channels[c]
-    clip_grids = np.clip(filter_cell_grids[:, i, :, :], v_min, v_max)
-    norm_filter_cell_grids[:, i, :, :] = (clip_grids- v_min) / (v_max - v_min)
+    zarr.save(f'{foldername}/cell_masks_level{zoom_level}_step{shift_step}.zarr', cell_boundries_grids)
+else:
+    # # filter out black item
+    v_mask = np.amax(norm_cell_grids, axis=(1,2,3))>0.5
+    # filter out non-cell grids
+    c_mask = np.amax(cell_boundries_grids, axis=(1,2,3))>0
+    mask = v_mask & c_mask
+    filter_cell_grids = cell_grids[mask]
+    patch_size = (filter_cell_grids.shape[0], len(selected_channel_index), h, w)
 
-zarr.save(f'{foldername}/cell_grids_level{zoom_level}_step{shift_step}_filter.zarr', norm_filter_cell_grids)
-zarr.save(f'{foldername}/cell_masks_level{zoom_level}_step{shift_step}_filter.zarr', cell_boundries_grids[mask])
+    norm_filter_cell_grids = zarr.zeros(patch_size, chunks=(
+        1000, None, None, None), dtype='float32')
+    for i,c in enumerate(selected_channels):
+        v_min, v_max = selected_channels[c]
+        clip_grids = np.clip(filter_cell_grids[:, i, :, :], v_min, v_max)
+        norm_filter_cell_grids[:, i, :, :] = (clip_grids- v_min) / (v_max - v_min)
+
+    zarr.save(f'{foldername}/cell_grids_level{zoom_level}_step{shift_step}_filter.zarr', norm_filter_cell_grids)
+    zarr.save(f'{foldername}/cell_masks_level{zoom_level}_step{shift_step}_filter.zarr', cell_boundries_grids[mask])
 
 # %%
 # visualize
@@ -144,7 +149,7 @@ def visualize(a, mask):
 # %%
 grid_filename = f'{foldername}/cell_grids_level{zoom_level}_step{shift_step}'
 mask_filename = f'{foldername}/cell_masks_level{zoom_level}_step{shift_step}'
-filter = True
+
 
 if filter:
     z = zarr.open(f'{grid_filename}_filter.zarr', mode='r')
